18dc566c4a7d298cc2cfb6a95e818a419ec316e5
pomdtr <achille@deta.sh>
2023-08-04 19:06:05 +0200

    update spec url

--- client.ts
+++ client.ts
@@ -1,7 +1,7 @@
 import { createClient, type NormalizeOAS } from "npm:fets";
-import openapi from "https://pomdtr-valTownSpec.web.val.run/openapi.ts";
+import openapi from "https://pomdtr-valtownOpenAPI.web.val.run";
 
-export const apiRoot = "https://api.val.town";
+export const apiRoot = openapi.servers[0].url;
 
 export const client = createClient<NormalizeOAS<typeof openapi>>({
   endpoint: apiRoot,
