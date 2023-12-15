b0585b1200298540a04b88df4f7242ac45740541
pomdtr <achille.lacoin@gmail.com>
2023-12-15 15:30:18 +0100

    update deps

--- deps.ts
+++ deps.ts
@@ -3,9 +3,9 @@ export {
   CompletionsCommand,
 } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
 export { Table } from "https://deno.land/x/cliffy@v1.0.0-rc.3/table/mod.ts";
-export * as path from "https://deno.land/std@0.208.0/path/mod.ts";
-export * as fs from "https://deno.land/std@0.208.0/fs/mod.ts";
-export { toText } from "https://deno.land/std@0.208.0/streams/to_text.ts";
+export * as path from "https://deno.land/std@0.209.0/path/mod.ts";
+export * as fs from "https://deno.land/std@0.209.0/fs/mod.ts";
+export { toText } from "https://deno.land/std@0.209.0/streams/to_text.ts";
 export { open } from "https://deno.land/x/open@v0.0.6/index.ts";
 export * as shlex from "npm:shlex";
 export { emphasize } from "npm:emphasize@6.0.0";
